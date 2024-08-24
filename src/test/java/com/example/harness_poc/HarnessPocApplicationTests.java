e8b6db16e616ba82de4f11de3e46f8f148ccf3a8
swastb <swastika10@gmail.com>
2024-08-24 13:20:14 +0530

    adding initial commit

---
+++ src/test/java/com/example/harness_poc/HarnessPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.harness_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarnessPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
